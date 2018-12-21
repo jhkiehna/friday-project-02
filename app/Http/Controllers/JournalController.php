30ccce72ea5a7f87602df3b1b230c1c5ade4a2f5
jhkiehna <jhkiehna@gmail.com>
2018-12-21 09:36:46 -0500

    started writing controller

---
+++ app/Http/Controllers/JournalController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JournalController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+}
