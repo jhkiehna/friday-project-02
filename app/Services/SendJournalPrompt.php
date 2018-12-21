a33b6d051f2e036c5c8bedbe3cfd32fabc71596f
jhkiehna <jhkiehna@gmail.com>
2018-12-21 11:35:59 -0500

    made first test pass for SendJournalPrompt

--- app/Services/SendJournalPrompt.php
+++ app/Services/SendJournalPrompt.php
@@ -9,6 +9,14 @@ class SendJournalPrompt
 {
     public function handle()
     {
+        $users = User::all();
 
+        $users->each(function ($user) {
+            $journalEntry = new JournalEntry([
+                'user_id' => $user->id
+            ]);
+
+            $user->journalEntries()->save($journalEntry);
+        });
     }
 }
--- database/migrations/2018_12_21_134430_create_journal_entries_table.php
+++ database/migrations/2018_12_21_134430_create_journal_entries_table.php
@@ -16,7 +16,7 @@ class CreateJournalEntriesTable extends Migration
         Schema::create('journal_entries', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
-            $table->integer('email_history_id');
+            $table->integer('email_history_id')->nullable();
             $table->text('content')->nullable();
             $table->timestamps();
         });
